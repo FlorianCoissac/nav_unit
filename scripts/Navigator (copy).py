4a8aeabb979f707f89d292f0c3ab737c8959039e
Florian Coissac <florian.coissac@visualbehavior.ai>
2022-09-09 15:50:28 +0200

    Upgraded Navigator for trajectory following with management of command superpositions

--- scripts/Navigator (copy).py
+++ scripts/Navigator (copy).py
@@ -2,7 +2,7 @@
 # license removed for brevity
 import rospy
 from std_msgs.msg import String
-from geometry_msgs.msg import PoseWithCovariance, Pose, PoseArray
+from geometry_msgs.msg import PoseWithCovariance, Pose
 from geometry_msgs.msg import Twist
 from nav_msgs.msg import Odometry
 from numpy import arctan2, pi, sqrt, cos
@@ -16,60 +16,20 @@ class Navigator():
         self.rate = rospy.Rate(30) # 10hz
         self.pos_with_co = PoseWithCovariance()
         self.pos = Pose()
-        self.goals=[]
         self.going_to_goal = False
-        self.interrupt = False
 
     def update_pos(self, odom_data):
         self.pos_with_co = odom_data.pose
         self.pos = self.pos_with_co.pose
 
-    def got_new_trajectory(self, trajectory):
-        if not self.going_to_goal:
-            self.goals = trajectory.poses
-            self.follow_trajectory()
-        else:
-            self.interrupt = True
-            rospy.sleep(0.5)
-            self.interrupt = False
-            self.goals = trajectory.poses
-            self.follow_trajectory()
-
-    def got_new_goal(self, goal):
-        if not self.going_to_goal:
-            self.go_to_goal(goal)
-        else:
-            self.interrupt = True
-            rospy.sleep(0.5)
-            self.interrupt = False
-            self.go_to_goal(goal)
-        
-    def follow_trajectory(self):
-        n_goal = 0
-        while n_goal<len(self.goals):
-            goal = self.goals[n_goal]
-            succeeded = self.go_to_goal(goal)
-            if succeeded:
-                rospy.loginfo("Goal reached : " + str(n_goal))
-                n_goal+=1
-            else:
-                self.goals = []
-                rospy.loginfo("Error : Trajectory supressed")
-        if succeeded:
-            rospy.loginfo("Trajectory completed")
-        else:
-            rospy.loginfo("Trajectory failed")
-
-
-
-    def go_to_goal(self, goal):
+    def navigation(self, goal):
         if not self.going_to_goal:
             self.going_to_goal = True
             # Init the Dx, Dy for the while loop check
             Dx = goal.position.x - self.pos.position.x
             Dy = goal.position.y - self.pos.position.y
             vel_msg = Twist()
-            while (Dx**2+Dy**2)>0.01 and not rospy.is_shutdown() and not self.interrupt:
+            while (Dx**2+Dy**2)>0.01 and not rospy.is_shutdown():
                 # Compute the vector towards goal
                 explicit_quat = [self.pos.orientation.x, self.pos.orientation.y, self.pos.orientation.z, self.pos.orientation.w]
                 roll, pitch, yaw = tf.transformations.euler_from_quaternion(explicit_quat)
@@ -94,7 +54,7 @@ class Navigator():
             explicit_quat = [goal.orientation.x, goal.orientation.y, goal.orientation.z, goal.orientation.w]
             goal_roll, goal_pitch, goal_yaw = tf.transformations.euler_from_quaternion(explicit_quat)
             Da = goal_yaw - yaw
-            while Da**2>0.01 and not rospy.is_shutdown() and not self.interrupt:
+            while Da**2>0.01 and not rospy.is_shutdown():
                 explicit_quat = [self.pos.orientation.x, self.pos.orientation.y, self.pos.orientation.z, self.pos.orientation.w]
                 roll, pitch, yaw = tf.transformations.euler_from_quaternion(explicit_quat)
                 explicit_quat = [goal.orientation.x, goal.orientation.y, goal.orientation.z, goal.orientation.w]
@@ -112,14 +72,12 @@ class Navigator():
             self.vel_publisher.publish(vel_msg)
             rospy.loginfo("Goal is reached !")
             self.going_to_goal = False
-            return True
 
 
     def listen(self):
         if True in ['/imu' in tops for tops in rospy.get_published_topics()]:
             rospy.loginfo("Navigator is listening for goal input")
-            rospy.Subscriber("/trajectory", PoseArray, self.got_new_trajectory)
-            rospy.Subscriber("/goal", Pose, self.got_new_goal)
+            rospy.Subscriber("goal", Pose, self.navigation)
             rospy.spin()
         else:
             rospy.loginfo("Odometry incomplete, please reconnect IMU sensor and execute 'roslaunch razor_imu_9dof razor-pub.launch' then try running Navigator.py again")
